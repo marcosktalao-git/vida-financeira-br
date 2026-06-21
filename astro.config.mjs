261ac022038c29dbe5dc1a289a2c59481c6de634
Marcos Henrique <seuemail@gmail.com>
2026-06-21 16:14:20 -0300

    chore: atualiza site para dominio proprio vidafinanceirabr.com.br

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://vida-financeira-br.vercel.app',
+  site: 'https://www.vidafinanceirabr.com.br',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
